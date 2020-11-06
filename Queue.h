ede707bc99ca57932138cc1e3b8fb717fa391570
Kashish Pandey <kpandey@chapman.edu>
2020-11-06 14:12:28 -0800

    Created Queue.h

---
+++ Queue.h
@@ -0,0 +1,101 @@
+#include "DoublyLinkedList.h" // from your code
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Queue{
+  // default constructor
+  Queue();
+  // overloaded constructor
+  Queue(int maxSize);
+  // destructor
+  ~Queue();
+
+  DoublyLinkedList<T> myQueue;
+
+  int front;
+  int rear;
+
+  // size of queue
+  int mSize;
+
+  // number of elements
+  int totalElements;
+
+  void insert(T *data);
+  // removing front element in queue
+  T* remove();
+  // checking if queue is empty
+  bool isEmpty();
+  // checking if queue is full
+  bool isFull();
+  // returning size of queue
+  int getSize();
+  // returns front element
+  T* peek();
+}
+
+//default constructor
+template<class T>
+Queue<T>::Queue(){
+  front = 0;
+  rear = -1;
+  totalElements = 0;
+}
+
+//overloaded constructor
+template<class T>
+Queue<T>::Queue(int maxSize){
+  front = 0;
+  rear = -1;
+  totalElements = 0;
+}
+
+//destructor
+template<class T>
+Queue<T>::~Queue(){
+  delete myQueue; // ? does this work ?
+}
+
+//insert element to back of queue
+template<class T>
+void Queue<T>::insert(T *data){
+  myQueue -> insertBack(data);
+  totalElements++;
+}
+
+// removing front element from queue
+template<class T>
+void Queue<T>::remove(){
+  totalElements--;
+  return myQueue -> removeFront();
+}
+
+//returns true or false - checking whether queue is empty
+template<class T>
+bool Queue<T>::isEmpty(){
+  if(totalElements == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//returns if true or false -  checking whether queue is full
+template<class T>
+bool Queue<T>::isFull(){
+  return(totalElements == mSize);
+}
+
+//returning first element of queue
+template<class T>
+T Queue<T>::peek(){
+  return myQueue -> peek();
+}
+
+// getting size of queue
+template<class T>
+T Queue<T>::getSize(){
+  return totalElements;
+}
